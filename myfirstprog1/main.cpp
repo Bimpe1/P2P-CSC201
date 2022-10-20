55f395dc5a1c2e00bbd39293651b836645caccc0
Bimpe1 <adebimpe.adetoba@pau.edu.ng>
2022-10-20 20:12:41 +0100

    Learning c++ started from introduction till Arrays

---
+++ myfirstprog1/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+/*  cout <<"   /|"<< endl;
+    cout <<"  / |"<< endl;
+    cout <<" /  |"<< endl;
+    cout <<"/___|"<< endl;
+    return 0;
+string characterName = "Tom";
+int characterAge;
+characterAge = 70;
+ cout <<"There once was a man named "<<characterName<< endl;
+ cout <<"He was 70 years old" << endl;
+ cout <<"He did not like the name George"<< endl;
+ cout <<"But he did not like being 70"<< endl;
+ char grade = 'A';
+ string phrase ="Giraffe Academy";
+ int age = 50;
+ double gpa = 2.3;
+ bool isMale= false;
+  cout << grade<< endl;
+    cout << "Hello\n";
+    cout << "grade"<< endl;
+    string phrase = "Giraffe Academy";
+    string phrasesub;
+    phrasesub = phrase.substr(9,4);
+    cout<< phrasesub;
+    return 0;
+    cout<< fmin(3,10);
+     string name;
+     cout<< "Enter your name: ";
+     getline(cin, name);
+     cout<<"Hello "<< name;
+    //BuILDING A CALCULATOR
+    double num1, num2;
+    cout<<"Enter first number";
+    cin>>num1;
+    cout<<"Enter second number";
+    cin>>num2;
+    cout<< num1+num2;*/
+    //Building a Mad Libs
+    string colour,pluralNoun,celebrity;
+    cout<<"Enter a colour: ";
+    getline(cin,colour);
+    cout<<"Enter a pluralNoun: ";
+    getline(cin,pluralNoun);
+    cout<<"Enter a celebrity: ";
+    getline(cin,celebrity);
+
+    cout<<"Roses are "<<colour<<endl;
+    cout<<pluralNoun<< " are blue"<<endl;
+    cout<<"I love " <<celebrity<<endl;
+
+
+    return 0;
+}
